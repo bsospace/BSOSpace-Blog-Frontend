f9ba73250b0d653bd802dc658c18de985c268bda
boytur <104257779+boytur@users.noreply.github.com>
2024-09-03 16:23:21 +0700

    ✨ feat[api/posts/[slug]/route.ts]: Add GET handler to fetch posts by user ID if slug is 'mypost'

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,38 +1,36 @@
-import { Tag } from "../../../interfaces/index";
-import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { verifyToken } from "@/app/utils/auth";
 
-// GET handler to fetch a single post by ID
+// GET handler to fetch posts by slug or by user ID if slug is 'mypost'
 export async function GET(req: NextRequest) {
   const slug = req.nextUrl.pathname.split("/").pop();
-
+  const token = req.cookies.get("auth-token")?.value;
   if (!slug) {
     return NextResponse.json({
       success: false,
-      message: "slug required!",
+      message: "Slug required!",
     });
   }
 
   try {
-    if (slug !== "all") {
-      const post = await prisma.post.findUnique({
-        where: { slug: slug },
-        include: {
-          Author: {
-            select: { name: true },
-          },
-          Category: true,
-          tags: true,
-        },
-      });
-
-      if (post) {
-        return NextResponse.json(post);
-      } else {
-        return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    if (slug === "my-posts") {
+      if (!token) {
+        return NextResponse.json(
+          { error: "Authentication required" },
+          { status: 401 }
+        );
       }
-    } else {
-      const post = await prisma.post.findMany({
+
+      // Decode user ID from token
+      const user = verifyToken(token) || null;
+
+      // Fetch posts created by the logged-in user
+      const posts = await prisma.post.findMany({
+        where: {
+          authorId: user?.id, // Add null check
+        },
         select: {
           id: true,
           title: true,
@@ -52,52 +50,53 @@ export async function GET(req: NextRequest) {
         },
       });
 
-      if (post) {
-        return NextResponse.json(post);
-      } else {
-        return NextResponse.json({ error: "Post not found" }, { status: 404 });
-      }
+      return posts.length > 0
+        ? NextResponse.json(posts)
+        : NextResponse.json({ error: "No posts found" }, { status: 404 });
+    } else if (slug !== "all") {
+      // Fetch a specific post by slug
+      const post = await prisma.post.findUnique({
+        where: { slug: slug },
+        include: {
+          Author: {
+            select: { name: true },
+          },
+          Category: true,
+          tags: true,
+        },
+      });
+
+      return post
+        ? NextResponse.json(post)
+        : NextResponse.json({ error: "Post not found" }, { status: 404 });
+    } else {
+      // Fetch all posts
+      const posts = await prisma.post.findMany({
+        select: {
+          id: true,
+          title: true,
+          slug: true,
+          createdAt: true,
+          Category: true,
+          Author: {
+            select: {
+              name: true,
+            },
+          },
+          tags: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      });
+
+      return posts.length > 0
+        ? NextResponse.json(posts)
+        : NextResponse.json({ error: "No posts found" }, { status: 404 });
     }
   } catch (error) {
     console.log(error);
-    return NextResponse.json(
-      { error: "Something went wrong" },
-      { status: 500 }
-    );
-  }
-}
-
-// PUT handler to update a post by ID
-// export async function PUT(req: NextRequest) {
-//   const id = req.nextUrl.searchParams.get("id");
-//   const { title, content, author, categoryId } = await req.json();
-
-//   try {
-//     const post = await prisma.post.update({
-//       where: { id: Number(id) },
-//       data: { title, content, author, categoryId: Number(categoryId) },
-//     });
-
-//     return NextResponse.json(post);
-//   } catch (error) {
-//     return NextResponse.json(
-//       { error: "Something went wrong" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-// DELETE handler to delete a post by ID
-export async function DELETE(req: NextRequest) {
-  const id = req.nextUrl.searchParams.get("id");
-
-  try {
-    await prisma.post.delete({
-      where: { id: Number(id) },
-    });
-
-    return NextResponse.json({}, { status: 204 });
-  } catch (error) {
     return NextResponse.json(
       { error: "Something went wrong" },
       { status: 500 }
