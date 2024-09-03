e9f037a083dc01ecaba2bf41d5b3deee43f4a659
boytur <104257779+boytur@users.noreply.github.com>
2024-09-03 16:21:10 +0700

    ✨ feat[posts.action.ts]: Add fetchPostById function and api enpoint

--- app/_action/posts.action.ts
+++ app/_action/posts.action.ts
@@ -17,6 +17,22 @@ export async function fetchPostBySlug(slug: string): Promise<Post | null> {
   }
 }
 
+export async function fetchPostById(id: number): Promise<Post | null> {
+  try {
+    const res = await fetch(`${process.env.PRODUCTION_URL}/api/posts/id/${id}`, {
+      next: {
+        revalidate: 0,
+      },
+      method: "GET",
+    });
+    const data: Post = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
 export async function fetchPosts(): Promise<Post[] | []> {
   try {
     const response = await fetch(
