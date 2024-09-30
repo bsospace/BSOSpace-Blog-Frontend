cc5c0cf26fc33315bed57609d898def316c88b76
boytur <104257779+boytur@users.noreply.github.com>
2024-09-30 19:27:58 +0700

    ✨ feat[api/posts]: add key query and make posts private if key is provided

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -6,6 +6,7 @@ import { verifyToken } from "@/app/utils/auth";
 // GET handler to fetch posts by slug or by user ID if slug is 'mypost'
 export async function GET(req: NextRequest) {
   const slug = req.nextUrl.pathname.split("/").pop();
+  const key = req.nextUrl.searchParams.get("key");
   const token = req.cookies.get("auth-token")?.value;
   if (!slug) {
     return NextResponse.json({
@@ -55,7 +56,7 @@ export async function GET(req: NextRequest) {
         : NextResponse.json({ error: "No posts found" }, { status: 404 });
     } else if (slug !== "all") {
       // Fetch a specific post by slug
-      const post = await prisma.post.findUnique({
+      let post = await prisma.post.findUnique({
         where: { slug: slug },
         include: {
           Author: {
@@ -66,9 +67,50 @@ export async function GET(req: NextRequest) {
         },
       });
 
-      return post
-        ? NextResponse.json(post)
-        : NextResponse.json({ error: "Post not found" }, { status: 404 });
+      // console.log("Post: ", post);
+
+      if (!post) {
+        return NextResponse.json({ error: "Post not found" }, { status: 404 });
+      }
+
+      if (post.published) {
+        // If post is published, return the post
+        return NextResponse.json({ post });
+      }
+
+      // If the post is not published
+      if (!post.published) {
+        if (!key) {
+          console.log("Key not found", true);
+
+          console.log("Post fomat: ", post.title);
+
+          const formattedPost = {
+            ...post,
+            content: "This post is not published yet!",
+            key: "",
+          };
+
+          return NextResponse.json({ post: formattedPost });
+        } else if (key !== null && key !== post.key) {
+          const formattedPost = {
+            ...post,
+            content: "This post is not published yet!",
+            key: "",
+          };
+
+          return NextResponse.json({
+            error: "คีย์ไม่ถูกต้อง",
+            post: formattedPost,
+          });
+        } else if (key === post.key) {
+          post = {
+            ...post,
+            published: true,
+          };
+          return NextResponse.json({ post });
+        }
+      }
     } else {
       // Fetch all posts
       const posts = await prisma.post.findMany({
