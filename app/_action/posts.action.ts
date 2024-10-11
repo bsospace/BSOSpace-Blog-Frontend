ba09ad9994c4cf625e53c2afc67f50914b24d9f1
Natnawat Panisarasirikul <34980328+LordEaster@users.noreply.github.com>
2024-10-11 13:57:43 +0700

    ✨ feat[home posts.action]: add pagination on homepage

--- app/_action/posts.action.ts
+++ app/_action/posts.action.ts
@@ -43,9 +43,22 @@ export async function fetchPostById(id: number): Promise<Post | null> {
   }
 }
 
-export async function fetchPosts(): Promise<Post[] | []> {
+export async function fetchPosts(
+  page: number,
+  limit: number
+): Promise<{ 
+  data: Post[]; 
+  pagination: { 
+    total_records: number; 
+    current_page: number; 
+    total_pages: number; 
+    next_page: number | null; 
+    prev_page: number | null 
+  } 
+}> {
   try {
     const response = await fetch(
+      // todo รอแก้ไขดึงข้อมูลจาก backend เตรียม path ยิงให้แล้ว ?page=${page}&limit=${limit}
       `${process.env.PRODUCTION_URL}/api/posts/all`,
       {
         next: {
@@ -54,10 +67,46 @@ export async function fetchPosts(): Promise<Post[] | []> {
         method: "GET",
       }
     );
-    return response.json();
+
+    // todo แก้ขัด รอแก้ไขดึงข้อมูลจาก backend แก้อันบนเสร็จแล้วฝากลบอันนี้ทิ้งด้วย
+    // pagination และ sort ตามวันที่
+    let resData = await response.json();
+    let total = resData.length;
+    let total_pages = Math.ceil(total / limit);
+    let next_page: number | null;
+    let prev_page: number | null;
+    
+    resData.sort((a: Post, b: Post) => {
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+    });
+
+    resData = resData.slice((page - 1) * limit, page * limit);
+
+    next_page = page + 1;
+    prev_page = page - 1;
+    
+    return { 
+      data: resData, 
+      pagination: {
+        total_records: total,
+        current_page: page,
+        total_pages: Math.ceil(total / limit),
+        next_page: next_page > total_pages ? null : next_page,
+        prev_page: prev_page > 0 ? prev_page : null,
+      },
+    };
   } catch (error) {
     console.error(error);
   }
 
-  return [];
+  return {
+    data: [],
+    pagination: {
+      total_records: 0,
+      current_page: 0,
+      total_pages:0,
+      next_page: null,
+      prev_page: null,
+    },
+  }
 }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,21 +1,57 @@
+'use client';
+
+import { useEffect, useState } from "react";
 import { fetchPosts } from "../_action/posts.action";
 import BlogCard from "../components/Blog";
 import { Post } from "../interfaces";
-
 interface Props {
   posts: Post[];
   error?: string;
 }
 
-export default async function HomePage() {
-  let posts: Post[] = [];
+export default function HomePage() {
+  const [posts, setPosts] = useState<Post[]>([]);
   let error: string | undefined;
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+  const [nextPage, setNextPage] = useState<number | null>(null);
+  const [prevPage, setPrevPage] = useState<number | null>(null);
+  const [indexStart, setIndexStart] = useState(0);
+  const [indexEnd, setIndexEnd] = useState(5);
+  const [total, setTotal] = useState(0);
 
-  try {
-    posts = await fetchPosts();
-  } catch (err) {
-    error = "Failed to load posts.";
+  async function getPosts() {
+    try {
+      let temp = await fetchPosts(page, limit);
+
+      setPosts(temp.data);
+      setIndexStart((page - 1) * limit + 1);
+      setIndexEnd((page * limit > temp.pagination.total_records) ? temp.pagination.total_records : page * limit);
+      setTotal(temp.pagination.total_records);
+      setNextPage(temp.pagination.next_page);
+      setPrevPage(temp.pagination.prev_page);
+    } catch (err) {
+      error = "Failed to load posts.";
+    }
   }
+
+  const handleNext = () => {    
+    if (nextPage == null) return;
+    setPage(page + 1);
+  }
+
+  const handlePrevious = () => {
+    if (prevPage == null) return;
+    setPage(page - 1);
+  }
+
+  const handleLimitChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLimit(parseInt(e.target.value));
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [limit, page]);
 
   if (error) {
     return (
@@ -55,6 +91,31 @@ export default async function HomePage() {
 
       <section>
         <h3 className="text-3xl font-bold text-gray-800 mb-6">Blogs</h3>
+        {/* pagination for blog */}
+        <div className="flex justify-between items-center mb-6">
+          <div>
+            <select className="border border-gray-300 rounded-md px-2 py-1" onChange={handleLimitChange}>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+            </select>
+          </div>
+          <div>
+            {/* 1 of n posts */}
+            <p className="text-gray-500 text-sm">
+              {indexStart} - {indexEnd} of {total} posts
+            </p>
+          </div>
+          <div>
+            <button onClick={handlePrevious} className={`${prevPage ? "bg-blue-500 text-white" : "bg-[#bfdbfe] text-[#808390]"} px-4 py-2 rounded-md`}>
+              Previous
+            </button>
+            <button onClick={handleNext} className={`${nextPage ? "bg-blue-500 text-white" : "bg-[#bfdbfe] text-[#808390]"} px-4 py-2 rounded-md ml-2`}>
+              Next
+            </button>
+          </div>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {posts.length > 0 ? (
             posts.map((post) => (
