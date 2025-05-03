a44ff32be6213a2bf8eebeced0457eba6512e11e
boytur <mask30254@gmail.com>
2025-05-03 20:19:12 +0700

    refactor(posts): switch to axios for fetching posts and update response structure

--- app/_action/posts.action.ts
+++ app/_action/posts.action.ts
@@ -1,5 +1,6 @@
-"use server";
-import { Post } from "@/app/interfaces";
+"use client";
+import { Meta, Post } from "@/app/interfaces";
+import { axiosInstance } from "../utils/api";
 
 export async function fetchPostBySlug(slug: string, key: string = "") {
   try {
@@ -43,70 +44,37 @@ export async function fetchPostById(id: number): Promise<Post | null> {
   }
 }
 
+export interface PostResponse {
+  posts: Post[];
+  meta: Meta;
+}
+
 export async function fetchPosts(
-  page: number,
-  limit: number
-): Promise<{ 
-  data: Post[]; 
-  pagination: { 
-    total_records: number; 
-    current_page: number; 
-    total_pages: number; 
-    next_page: number | null; 
-    prev_page: number | null 
-  } 
-}> {
+  page: number = 1,
+  limit: number = 10,
+  search: string = ""
+): Promise<{ data: Post[]; meta: Meta }> {
   try {
-    const response = await fetch(
-      // todo รอแก้ไขดึงข้อมูลจาก backend เตรียม path ยิงให้แล้ว ?page=${page}&limit=${limit}
-      `${process.env.PRODUCTION_URL}/api/posts/all`,
-      {
-        next: {
-          revalidate: 0,
-        },
-        method: "GET",
-      }
+    const resposne = await axiosInstance.get<PostResponse>(
+      `/posts?page=${page}&limit=${limit}&search=${search}`
     );
 
-    // todo แก้ขัด รอแก้ไขดึงข้อมูลจาก backend แก้อันบนเสร็จแล้วฝากลบอันนี้ทิ้งด้วย
-    // pagination และ sort ตามวันที่
-    let resData = await response.json();
-    let total = resData.length;
-    let total_pages = Math.ceil(total / limit);
-    let next_page: number | null;
-    let prev_page: number | null;
-    
-    resData.sort((a: Post, b: Post) => {
-      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-    });
-
-    resData = resData.slice((page - 1) * limit, page * limit);
-
-    next_page = page + 1;
-    prev_page = page - 1;
-    
-    return { 
-      data: resData, 
-      pagination: {
-        total_records: total,
-        current_page: page,
-        total_pages: Math.ceil(total / limit),
-        next_page: next_page > total_pages ? null : next_page,
-        prev_page: prev_page > 0 ? prev_page : null,
-      },
+    return {
+      data: resposne.data.posts,
+      meta: resposne.data.meta,
     };
   } catch (error) {
-    console.error(error);
-  }
+    console.error("Failed to fetch posts:", error);
 
-  return {
-    data: [],
-    pagination: {
-      total_records: 0,
-      current_page: 0,
-      total_pages:0,
-      next_page: null,
-      prev_page: null,
-    },
+    return {
+      data: [],
+      meta: {
+        total: 0,
+        hasNextPage: false,
+        page: 0,
+        limit: 0,
+        totalPage: 0,
+      },
+    };
   }
 }
