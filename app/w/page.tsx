8ba25b19da135aefac05ff085bb5946dafc7e7b8
boytur <mask30254@gmail.com>
2025-05-22 08:52:57 +0700

    feat: add custom hooks for mobile detection, window size tracking, and Tiptap editor integration
    - Introduced `useMobile` hook for mobile device detection based on viewport width.
    - Added `useWindowSize` hook to track window dimensions and viewport offset.
    - Created `useTiptapEditor` hook to manage Tiptap editor instances.
    - Implemented utility functions in `tiptap-utils.ts` for schema checks and image upload handling.
    - Added global styles in `index.scss` and keyframe animations in `_keyframe-animations.scss`.
    - Defined CSS variables for theming in `_variables.scss`.
    - Updated page component to utilize new Tiptap editor and added necessary imports.

--- app/w/page.tsx
+++ app/w/page.tsx
@@ -1,9 +1,11 @@
+'use client';
 import TiptapEditor from "@/app/components/TiptapEditor"
+import { SimpleEditor } from "../components/tiptap-templates/simple/simple-editor"
 
 export default function Page() {
     return (
-        <div className="p-4 w-full relative rounded-lg bg-white dark:bg-zinc-900 shadow-md dark:bg-[#1F1F1F]">
+        <>
             <TiptapEditor />
-        </div>
+        </>
     )
 }
