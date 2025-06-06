45410529157884bc8ec3384af6bebf1082971614
boytur <mask30254@gmail.com>
2025-06-06 14:51:06 +0700

    chore: simplify environment configuration by removing unused variables and updating required fields

--- app/configs/envConfig.ts
+++ app/configs/envConfig.ts
@@ -1,63 +1,16 @@
-
 interface IEvnConfig {
     [key: string]: string | number | undefined;
+    nodeEnv: string;
     apiBaseUrl: string;
     openIdApiUrl: string;
     callBackUrl: string;
-    nodeEnv: string;
-    port: number;
-    developmentUrl?: string;
-    jwtSecret: string;
-    databaseUrl: string;
-    databasePort: number;
-    databaseName: string;
-    databaseUser: string;
-    databasePassword: string;
-    googleClientId?: string;
-    googleClientSecret?: string;
-    gitHubClientId?: string;
-    gitHubClientSecret?: string;
-    discordClientId?: string;
-    discordClientSecret?: string;
-    cloudflareR2BucketName: string;
-    cloudflareR2AccessKey: string;
-    cloudflareR2SecretKey: string;
-    cloudflareR2AccessId: string;
-    cloudflareR2Domain: string;
-    destinationBucketName?: string;
-    destinationAccountId?: string;
-    destinationAccessKey?: string;
-    destinationSecretKey?: string;
 }
 
 const envConfig: IEvnConfig = {
     nodeEnv: process.env.NODE_ENV || 'development',
     apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || '',
-    port: parseInt(process.env.APP_PORT || '3000', 10),
-    developmentUrl: process.env.DEVELOPMENT_URL || '',
     openIdApiUrl: process.env.NEXT_PUBLIC_OPENID_API_URL || '',
-    callBackUrl: process.env.NEXT_PUBLIC_CALLBACK_URL || '',
-    jwtSecret: process.env.JWT_SECRET || '',
-    databaseUrl: process.env.DATABASE_URL || '',
-    databasePort: parseInt(process.env.DATABASE_PORT || '5432', 10),
-    databaseName: process.env.DATABASE_NAME || '',
-    databaseUser: process.env.PG_USER || '',
-    databasePassword: process.env.PG_PASSWORD || '',
-    googleClientId: process.env.GOOGLE_CLIENT_ID || '',
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    gitHubClientId: process.env.GITHUB_CLIENT_ID || '',
-    gitHubClientSecret: process.env.GITHUB_CLIENT_SECRET || '',
-    discordClientId: process.env.DISCORD_CLIENT_ID || '',
-    discordClientSecret: process.env.DISCORD_CLIENT_SECRET || '',
-    cloudflareR2BucketName: process.env.R2_BUCKET_NAME || '',
-    cloudflareR2AccessKey: process.env.R2_ACCESS_KEY || '',
-    cloudflareR2SecretKey: process.env.R2_SECRET_KEY || '',
-    cloudflareR2AccessId: process.env.R2_ACCOUNT_ID || '',
-    cloudflareR2Domain: process.env.R2_DOMAIN || '',
-    destinationBucketName: process.env.DESTINATION_BUCKET_NAME || '',
-    destinationAccountId: process.env.DESTINATION_ACCOUNT_ID || '',
-    destinationAccessKey: process.env.DESTINATION_ACCESS_KEY || '',
-    destinationSecretKey: process.env.DESTINATION_SECRET_KEY || ''
+    callBackUrl: process.env.NEXT_PUBLIC_CALLBACK_URL || ''
 };
 
 console.table(envConfig);
@@ -66,13 +19,9 @@ export const checkEnvConfig = () => {
     // Define only the required fields
     const requiredFields: string[] = [
         'nodeEnv',
-        'port',
-        'jwtSecret',
+        'apiBaseUrl',
         'openIdApiUrl',
-        'databaseUrl',
-        'databaseName',
-        'databaseUser',
-        'databasePassword'
+        'callBackUrl'
     ];
 
     for (const field of requiredFields) {
